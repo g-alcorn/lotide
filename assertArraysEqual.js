783a2b8cc9d7345394179ea1b26945a590325272
g-alcorn <griffin.alcorn@gmail.com>
2019-09-17 19:54:26 +0000

    successfully compares arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(array1, array2) {
+  var result = false;
+  if (array1.length !== array2.length)
+    result =false;
+
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i])
+      result = true;
+    else {
+      result = false;
+      break;
+    }
+  }
+  if (result === true)
+    console.log("The arrays are equal.");
+  else
+    console.log("The arrays are not equal.");
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); //test arrays are equal
+eqArrays([1, 2, 3], [1, 3, 3]); //test arrays are unequal
