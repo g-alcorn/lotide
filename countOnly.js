497a8cc97c3910baf59e709501f360ff39b6961e
g-alcorn <griffin.alcorn@gmail.com>
2019-09-18 16:37:52 +0000

    first attempt - failed

---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰Assertion passed: ${actual} === ${expected}`);
+    return;
+  } else {
+    console.log(`🤬Assertion failed: ${actual} !== ${expected}`);
+    return;
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const response = {};
+
+  for (var items in itemsToCount) {
+    if (itemsToCount[items]) {
+      response[allItems] += 1;
+    }
+  }
+  return response;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
