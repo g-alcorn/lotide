e8d020db931c12292ca0ccb84bbf48d6f877c8b6
g-alcorn <griffin.alcorn@gmail.com>
2019-09-20 00:25:06 +0000

    functional with tests implemented

--- findKey.js
+++ findKey.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🥰Assertion passed: ${actual} === ${expected}`);
+    console.log(`😍Assertion passed: ${actual} === ${expected}`);
     return;
   } else {
     console.log(`🤬Assertion failed: ${actual} !== ${expected}`);
@@ -11,38 +11,39 @@ const assertEqual = function(actual, expected) {
 const findKey = function(object, callback) {
   let keys = Object.keys(object), innerKeys = [], stars = [];
   let response = undefined;
-  console.log(keys);
+
   for (var i = 0; i < keys.length; i++) {
     innerKeys.push(Object.keys(object[keys[i]]));
   }
 
-  console.log(innerKeys);
-  // for (var i = 0; i < innerKeys.length;)
-
   for (var i = 0; i < innerKeys.length; i ++) {
     for (var j = 0; j < innerKeys[i].length; j++) {
-      stars.push(object[keys[i][innerKeys[j]]]);
+      stars.push(object[keys[i]][innerKeys[0][0]]);
     }
   }
 
-  console.log(stars);
+ 
   for (var i = 0; i < stars.length; i++) {
-    console.log(stars[i]);
     if (callback(stars[i])) {
-      response = object[keys[i][innerKeys[i][deepKeys[i]]]]; 
+      response = stars[i]; 
+      console.log(keys[i] + " has the correct number of stars!");
       return response;
     }
   }
 
+  console.log("No truthy value found");
   return response;
 };
 
 
-findKey({
+let input = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x === 2) // => "noma"
+}
+assertEqual(findKey(input , x => x === 2), 2); // => "noma"
+assertEqual(findKey(input , x => x === 3), 3); // => "Akaleri"
+assertEqual(findKey(input , x => x === 5), 5); // => "undefined"
