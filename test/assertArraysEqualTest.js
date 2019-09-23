7331ecb44d9dcb3ab4cdec815ad95d84cf06d37c
g-alcorn <griffin.alcorn@gmail.com>
2019-09-23 18:18:38 +0000

    brewed the chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,3 +1,12 @@
 const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual([1, 2, 3], [1, 2, 3]); //test arrays are equal
-assertArraysEqual([1, 2, 3], [1, 3, 3]); //test arrays are unequal
+const assert = require('chai').assert;
+
+describe("testing asserting arrays equal", () => {
+  it("should return true when passed [1, 2, 3] and [1, 2, 3]", () => {
+    assert.isTrue(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
+
+  it("should return false when passed [1, 2, 3] and [1, 3, 2]", () => {
+    assert.isFalse(assertArraysEqual([1, 2, 3], [1, 3, 2]));
+  });
+});
