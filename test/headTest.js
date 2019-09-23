e2271448a032444c0545bbec5df95c6861f91e86
g-alcorn <griffin.alcorn@gmail.com>
2019-09-23 18:06:07 +0000

    brewed the chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,17 @@
-const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
 //testing
-assertEqual(head([5,6,7]), 7);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "hello");
+describe("testing head of array", () => {
+  it("should return 5 when given [5, 6, 7]", () => {
+    assert.isTrue(head([5, 6, 7]) === 5);
+  });
+
+  it("should return 'hello' when given ['hello', 'lighthouse', 'labs']", () => {
+    assert.isTrue(head(["hello", "lighthouse", "labs"]) === "hello");
+  });
+
+  it("should return 'h' when given 'hello'", () => {
+    assert.isTrue(head("hello") === "h");
+  });
+});
