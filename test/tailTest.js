a16fd9b02937ee0eb5ee7074a9ca82461c8c7263
g-alcorn <griffin.alcorn@gmail.com>
2019-09-23 17:51:24 +0000

    updated w chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,18 @@
-const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
 const tail = require('../tail');
+const assert = require('chai').assert;
+
 
 //testing
 const testInput = ["Hello","Lighthouse","Labs"];
 const test1 = tail(testInput);
-assertEqual(test1.length, 2);
-assertEqual(test1[0], "Lighthouse");
-assertEqual(testInput.length,3);
+
+describe("testing tail", () => {
+  it("should return ['Lighthouse', 'Labs'] when given ['Hello', 'Lighthouse', 'Labs'", () => {
+    assert.isTrue(eqArrays(["Lighthouse", "Labs"], test1));
+  });
+
+  it("should return array with length one less than input", () => {
+    assert.isTrue(test1.length === testInput.length - 1);
+  });
+});
